cebba6060555e5db631614542ff880e03fd100a2
Wildcrasher <dzikigeneral1996@o2.pl>
2017-06-28 21:46:24 +0200

    projekt

---
+++ pl_szkielet06_win/pl_szkielet06_win/czolg.h
@@ -0,0 +1,34 @@
+// This is a .h file for the model: czolg
+
+// Positions: 19504
+// Texels: 20008
+// Normals: 4309
+// Faces: 10274
+// Vertices: 30822
+#ifndef TANK_H
+#define TANK_H
+
+#include "model.h"
+
+
+namespace Models {
+	namespace TankInternal {
+
+	extern unsigned int czolgVertices;
+    extern float czolgPositions[92466];
+    extern float czolgTexels[61644];
+    extern float czolgNormals[92466];
+	}
+
+	class Tank: public Model {
+		public:
+			Tank();
+			virtual ~Tank();
+			virtual void drawSolid();
+	};
+
+	extern Tank tank;
+}
+
+
+#endif
