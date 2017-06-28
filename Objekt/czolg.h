858b0196f5a1f87cf2641a03d7a1753558c74a91
Piotrekf3 <piotrek.fur@wp.pl>
2017-06-28 18:56:21 +0200

    czolg.h

---
+++ Objekt/czolg.h
@@ -0,0 +1,13 @@
+// This is a .h file for the model: czolg
+
+// Positions: 19504
+// Texels: 20008
+// Normals: 4309
+// Faces: 10274
+// Vertices: 30822
+
+const int czolgVertices;
+const float czolgPositions[92466];
+const float czolgTexels[61644];
+const float czolgNormals[92466];
+
